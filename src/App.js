56b8a73d14c2542d7dc7b9895633ba828e92e8a7
Chanakya sarma <chanakyasarmakashyap@gmail.com>
2023-01-18 00:15:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,18 +15,13 @@ class App extends React.Component{
       .catch((error)=>{
         console.log(error);
       });
-      gettime=()=>{
-        var dt = new Date();
-        document.getElementById('date-time').innerHTML=dt;
-      }
-
+      
     }
     render(){
       const{advice}=this.state;
       return(
         
         <div className="app">
-          <span id='date-time'></span>
           <div className="card">
             <h1 className="heading">{advice}</h1>
             <button className="button" onClick={this.fetchAdvice}>
